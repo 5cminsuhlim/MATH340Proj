b14dab0295c295fd245c30fbd62fb5e49efc5952
alinlzx <89933411+alinlzx@users.noreply.github.com>
2022-11-30 01:14:52 -0500

    Update misc.R

--- code/Math 340 Proj misc/misc.R
+++ code/Math 340 Proj misc/misc.R
@@ -1,4 +1,33 @@
+
+#creating a dataset that merges games by player-year
+
+playeryear <- summarize(group_by(players, Name, Time),
+                        mean_acpl = mean(Mean_CP),
+                        mean_sdcpl = mean(Std_CP),
+                        win_ratio = mean(WL),
+                        Elo = mean(Elo))
+
+########################
+
 library(ggplot2)
+
+ggplot(data = playeryear[playeryear$Elo != 0,]) + geom_point(aes(x = Elo, y = mean_sdcpl, color = mean_acpl)) +
+  theme_minimal() + scale_color_gradient(low = "lightblue", high = "darkred")
+
+#sd weighted least squares
+
+sd.lm <- lm(mean_sdcpl ~ Elo + win_ratio, data = playeryear[playeryear$Elo != 0,])
+summary(sd.lm)
+
+plot(playeryear[playeryear$Elo != 0,]$win_ratio, abs(sd.lm$resid))
+
+
+
+
+
+
+
+#####################################
 
 niemann$WL <- factor(niemann$WL, levels = c("0","1/2","1"))
 
@@ -14,3 +43,7 @@ summary(carlsen.ord)
 ggplot(data = players) + geom_point(aes(x = Elo, y = Mean_CP, color = Name)) + theme_minimal()
 
 ggplot(data = players) + geom_point(aes(x = Elo, y = Mean_CP, color = Time)) + theme_minimal()
+
+#transform data
+
+hist(players$Mean_CP)
