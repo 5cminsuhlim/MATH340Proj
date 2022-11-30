63cdb85046514f4633aaf5154dc3b1bf0f681286
alinlzx <89933411+alinlzx@users.noreply.github.com>
2022-11-30 13:00:58 -0500

    added manova

--- code/Math 340 Proj misc/misc.R
+++ code/Math 340 Proj misc/misc.R
@@ -1,3 +1,74 @@
+#ANOVA & MANOVA
+
+##Data Processing
+
+players.2 <- filter(players, players$Elo != 0) #filtering out players with 0 elo
+players.2 <- players.2[,c(4:9, 11)]
+
+players.niemann <- players.2
+players.niemann$Name <- ifelse(players.niemann$Name=="niemann", "niemann","other") #dataset to compare niemann vs others
+
+##Checking Assumptions
+
+players.2$mahal <- mahalanobis(players.2[,-7], colMeans(players.2[,-7]), cov(players.2[,-7])) #checking for multivariate outliers
+players.2$mahal_p <- pchisq(players.2$mahal, df=5, lower.tail=FALSE) #a lot of outliers, so assumptions technically not met? 
+
+##MANVOA with ACPL and SDCPL for all players
+
+players.manova <- manova(as.matrix(players.2[,2:3]) ~ players.2$Name)
+summary(players.manova)
+summary(players.manova, test = "Wilks")
+summary(players.manova, test = "Hotelling-Lawley")
+summary(players.manova, test = "Roy")
+
+##MANVOA with ACPL and SDCPL for Niemann vs the rest
+
+players.niemann.manova <- manova(as.matrix(players.niemann[,2:3]) ~ players.niemann$Name)
+summary(players.niemann.manova)
+summary(players.niemann.manova, test = "Wilks")
+summary(players.niemann.manova, test = "Hotelling-Lawley")
+summary(players.niemann.manova, test = "Roy")
+
+##MANVOA with ACPL and SDCPL with multiple IVs
+
+players.niemann.manova.twoway <- manova(as.matrix(players.niemann[,2:3]) ~ players.niemann$Name * as.factor(players.niemann$Time))
+summary(players.niemann.manova.twoway)
+
+###setting up elo as a categorical variable
+
+players.niemann$elo.cat <- ifelse(players.niemann$Elo < 2500, "low",
+                                  ifelse(players.niemann$Elo >= 2500 & players.niemann$Elo < 2750, "med",
+                                        ifelse(players.niemann$Elo >= 2750, "high", "NA")))
+
+
+###MANOVA with ACPL and SDCPL with multiple IVs, including categorical elo
+
+players.niemann.manova.twoway.elo <- manova(as.matrix(players.niemann[,2:3]) ~ players.niemann$Name * as.factor(players.niemann$elo.cat))
+summary(players.niemann.manova.twoway.elo) # testing for interaction
+
+players.niemann.manova.twoway.elo <- manova(as.matrix(players.niemann[,2:3]) ~ players.niemann$Name + as.factor(players.niemann$elo.cat))
+summary(players.niemann.manova.twoway.elo) 
+
+summary.aov(players.niemann.manova.twoway.elo)
+
+###Some accompanying visualization
+
+ggplot() + geom_point(data = players.niemann[players.niemann$Name=="other",], aes(x = Mean_CP, y = Std_CP), color = "gray", alpha = 0.2) +
+  geom_point(data = players.niemann[players.niemann$Name=="niemann",], aes(x = Mean_CP, y = Std_CP, color = elo.cat)) + 
+  #scale_color_gradient(low = "white", high = "darkred") +
+  theme_minimal()
+
+#########################
+
+#Influence Measures
+
+players.lm <- lm(Std_CP ~ Elo + Time + WL, data = players.2)
+summary(players.lm)
+
+influence.players.lm <- influence.measures(players.lm)
+plot(players.lm, which = 4)
+
+#########################
 
 #creating a dataset that merges games by player-year
 
